e4ff368b9b0e8f42d9d2fafedf454bdb16eb2360
Vinit (Code Vinu) Vijal <vineet.22566@sscbs.du.ac.in>
2025-01-14 14:40:40 +0530

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'ShareCBS',
-    short_name: 'ShareCBs',
+    short_name: 'ShareCBS',
     description: 'A Progressive Web App built with ❤️ by Vinu',
     start_url: '/',
     display: 'standalone',
